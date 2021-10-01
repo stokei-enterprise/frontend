bc6e53a09018b832f862ceed07757dbc0f030b9c
stokei <stokei@users.noreply.github.com>
2021-10-01 03:14:18 +0000

    Prettified Code!

--- packages/dashboard/config/server/index.ts
+++ packages/dashboard/config/server/index.ts
@@ -3,4 +3,6 @@ import { NODE_ENV } from '~/environments';
 export const isProduction: boolean = NODE_ENV === 'production';
 export const isDevelopment: boolean = NODE_ENV === 'development';
 
-export const SERVER_URL = isProduction ? "https://painel.stokei.com" : "http://localhost:3000";
+export const SERVER_URL = isProduction
+  ? 'https://painel.stokei.com'
+  : 'http://localhost:3000';
